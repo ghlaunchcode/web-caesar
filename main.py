a0e19f9df277ed2a77a4f594b627c68ed739f561
pskt <geoff@megaman>
2017-09-24 02:22:21 -0500

    Update initial page skeleton (change style to inline)

--- main.py
+++ main.py
@@ -11,19 +11,33 @@ page_template = """
 <html>
     <head>
         <title>Web Caesar</title>
+        <style>
+            form {
+                background-color: #ccc;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
     </head>
     <body>
         <main>
             <section>
-                <div id="div_caesar">
-                    <form>
-                        <label>
+                <form method="POST">
+                        <label for="rot">
                             Rotate By:
                         </label>
-                        <input type="text"/>
+                        <input type="text" name="rot"/>
+                        <input type="textarea" name="text"/>
                         <input type="submit"/>
-                    </form>
-                </div>
+                </form>
             </section>
             <footer>
                 2017, Geoffrey Hadler
